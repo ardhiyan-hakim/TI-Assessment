f2832c96b3cf298c0b30df77dba63728d8596dcb
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2025-02-28 13:15:46 +0700

    Installing Sass and Route Adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  );
 }
 
 export default App;
